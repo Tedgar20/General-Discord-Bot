add9385d47f52fe0b8ba033b294c0c98cffaea43
chipeta,edgar <tedgar@protonmail.com>
2020-04-01 19:56:34 -0400

    Removed message auto delete

--- src/util/deletedReason.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    listResponse: ['This message has been removed due to recent high volumes to our servers. Have a Wagner day.',
-        'This message has been deleted because you do not have the firt name Dylan. Have a Wagner day.',
-        'This message has been deleted by the Bernie gang. Get Berned. Have a Wagner day.',
-        'This message has been deleted because you\'re aflaming faggot. Have a Wagner day.',
-        'This message has been deleted because I said so. Have a Wagner day.',
-        'Do you really think that\'s appropriate. Have a Wagner day.'
-    ]
-}
