1524dcfd11d031c273a07153c33b42c31dae4dc9
Edgar Chipeta <tedgar@protonmail.com>
2020-04-10 18:53:23 -0400

    Added more quotes

--- src/misc/quoteMap.js
+++ src/misc/quoteMap.js
@@ -1,5 +1,11 @@
 module.exports.dotaVoiceLines = {
     'Ice is nice!': 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b4/Vo_crystalmaiden_cm_spawn_04.mp3',
     'Outplayed': 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b8/Vo_nevermore_nev_deny_08.mp3',
-    'Someone once told me' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/4/4e/Vo_shredder_timb_rare_01.mp3'
+    'Someone once told me' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/4/4e/Vo_shredder_timb_rare_01.mp3',
+    'Shitty wizard' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/3/33/Vo_luna_luna_shiwiz_01.mp3',
+    'What hope' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b0/Vo_luna_luna_rare_03.mp3',
+    'I would water' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/d/da/Vo_luna_luna_rare_01.mp3',
+    'lol' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/3/38/Vo_luna_luna_laugh_01.mp3',
+    'Its in the bag mate' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/a/a7/Vo_bristleback_bristle_inthebag_01.mp3',
+    'From the Ghastly Eyrie' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/d/d5/Vo_skywrath_mage_drag_inthebag_01.mp3'
 }
