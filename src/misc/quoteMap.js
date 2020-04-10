309269e6eabe3cb4a3940f7393c6f11dfdbc6f47
Edgar Chipeta <tedgar@protonmail.com>
2020-04-10 18:24:01 -0400

    Added timbersaw line

--- src/misc/quoteMap.js
+++ src/misc/quoteMap.js
@@ -1,4 +1,5 @@
 module.exports.dotaVoiceLines = {
     'Ice is nice!': 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b4/Vo_crystalmaiden_cm_spawn_04.mp3',
-    'Outplayed': 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b8/Vo_nevermore_nev_deny_08.mp3'
+    'Outplayed': 'https://gamepedia.cursecdn.com/dota2_gamepedia/b/b8/Vo_nevermore_nev_deny_08.mp3',
+    'Someone once told me' : 'https://gamepedia.cursecdn.com/dota2_gamepedia/4/4e/Vo_shredder_timb_rare_01.mp3'
 }
